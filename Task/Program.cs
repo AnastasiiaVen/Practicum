4743a794f2a6d0f3983c15567e6c8e8f54d93646
Anastasiia <venanastasiia@gmail.com>
2022-02-02 20:38:05 +0300

    Добавили решение задачи

---
+++ Task/Program.cs
@@ -0,0 +1,31 @@
+﻿// Написать программу, которая из имеющегося массива целых чисел 
+// формирует массив из четных чисел. Первоначальный массив можно ввести 
+// с клавиатуры, либо сгенерировать случайным образом. При решении 
+// не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
+
+int[] Array = new int[10];
+int[] NewArray = new int[10];
+
+void CreatPrintArray(int[] Array)
+{
+  for (int i = 0; i < Array.Length; i++)
+  {
+    Array[i] = new Random().Next(1, 15);
+    Console.Write($"{Array[i]} ");
+  }
+}
+
+void ChangeArray(int[] Array)
+{
+  for (int i = 0; i < Array.Length; i++)
+  {
+    NewArray[i] = Array[i];
+    if (NewArray[i] >8)
+      System.Console.Write($"{NewArray[i]} ");
+  }
+}
+
+
+CreatPrintArray(Array);
+System.Console.WriteLine();
+ChangeArray(Array);
